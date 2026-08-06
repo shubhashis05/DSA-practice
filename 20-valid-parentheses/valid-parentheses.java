2875bfa6f4093b38e9d08ecc7835414e2aa51e87
Shubhashis Das <139637882+shubhashis05@users.noreply.github.com>
2026-08-06 21:18:10 +0530

    Time: 3 ms (86.07%) | Memory: 43.7 MB (10.19%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+        int len = s.length();
+        if(len %2 != 0) return false;
+        Stack<Character> st = new Stack();
+        char ch;
+        for(int i = 0 ; i< len ; i++){
+            ch = s.charAt(i);
+            if(ch == '(' || ch == '{' ||ch == '[')
+                st.push(ch);
+            else{
+                if(st.size() == 0) return false;
+                if(ch == '}'){
+                    if(st.peek() == '{') st.pop();
+                    else return false;
+                }
+                else if(ch == ')'){
+                    // if(st.size() == 0) return false;
+                    if(st.peek() == '(') st.pop();
+                    else return false;
+                }
+                else if(ch == ']'){
+                    // if(st.size() == 0) return false;
+                    if(st.peek() == '[') st.pop();
+                    else return false;
+                }
+            }
+        }
+        if(st.size() == 0) return true;
+        return false;
+    }
+}

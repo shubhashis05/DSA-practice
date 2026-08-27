d23b906e3644ee2ad1cc982ce81b44b263ed5c2d
Shubhashis Das <139637882+shubhashis05@users.noreply.github.com>
2026-08-27 21:37:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (32.61%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,44 +1,37 @@
+import java.util.*;
+
 class MyQueue {
-Stack<Integer> input ;
-Stack<Integer> output ;
+    Stack<Integer> input;
+    Stack<Integer> output;
+
     public MyQueue() {
         input = new Stack<>();
         output = new Stack<>();
     }
-    
+
     public void push(int x) {
         input.push(x);
     }
-    
+
     public int pop() {
-        while(!input.isEmpty())
-            output.push(input.pop());
-        int val = output.pop();
-        while(!output.isEmpty())
-            input.push(output.pop());
-        return val;
+        moveElements();
+        return output.pop();
     }
-    
+
     public int peek() {
-        while(!input.isEmpty())
-            output.push(input.pop());
-        int val = output.peek();
-        while(!output.isEmpty())
-            input.push(output.pop());
-        return val;
+        moveElements();
+        return output.peek();
     }
-    
+
     public boolean empty() {
-        if(input.isEmpty()) return true;
-        return false;
+        return input.isEmpty() && output.isEmpty();
+    }
+
+    private void moveElements() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
     }
 }
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */

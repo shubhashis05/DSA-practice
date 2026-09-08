701407a6fee9b0001cb2d581e0faa5d38933afe9
Shubhashis Das <139637882+shubhashis05@users.noreply.github.com>
2026-09-08 23:33:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (20.12%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -1,20 +1,23 @@
 class Solution {
-    static int totalSum;
-    public int sum(TreeNode root){
-        if(root == null) return 0;
-        return root.val+sum(root.left)+sum(root.right);
+    public int sum(TreeNode root) {
+        if (root == null)
+            return 0;
+        return root.val + sum(root.left) + sum(root.right);
     }
-    public void helper(TreeNode root){
-        if(root == null) return;
-        helper(root.left);
+
+    public void helper(TreeNode root, int[] totalSum) {
+        if (root == null)
+            return;
+        helper(root.left, totalSum);
         int temp = root.val;
-        root.val = totalSum;
-        totalSum -= temp;
-        helper(root.right);
+        root.val = totalSum[0];
+        totalSum[0] -= temp;
+        helper(root.right, totalSum);
     }
+
     public TreeNode bstToGst(TreeNode root) {
-        totalSum = sum(root);
-        helper(root);
+        int totalSum = sum(root);
+        helper(root, new int[] { totalSum });
         return root;
     }
 }

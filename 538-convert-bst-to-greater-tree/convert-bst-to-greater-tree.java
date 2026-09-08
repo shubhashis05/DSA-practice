eb2caff37e6b0f1290c2915b4e17a2430d1a8f67
Shubhashis Das <139637882+shubhashis05@users.noreply.github.com>
2026-09-08 23:43:50 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (62.11%) - LeetSync

--- 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
@@ -1,23 +1,16 @@
 class Solution {
-    public int sum(TreeNode root) {
-        if (root == null)
-            return 0;
-        return root.val + sum(root.left) + sum(root.right);
-    }
+    public void helper(TreeNode root, int[] x) {
+            if (root == null)
+                return;
+            helper(root.right, x);
+            root.val = x[0] + root.val;
+            x[0] = root.val;
+            helper(root.left, x);
+        }
 
-    public void helper(TreeNode root, int[] totalSum) {
-        if (root == null)
-            return;
-        helper(root.left, totalSum);
-        int temp = root.val;
-        root.val = totalSum[0];
-        totalSum[0] -= temp;
-        helper(root.right, totalSum);
-    }
-
-    public TreeNode convertBST(TreeNode root) {
-        int totalSum = sum(root);
-        helper(root, new int[] { totalSum });
-        return root;
-    }
+        public TreeNode convertBST(TreeNode root) {
+            int[] arr = {0};
+            helper(root, arr);
+            return root;
+        }
 }

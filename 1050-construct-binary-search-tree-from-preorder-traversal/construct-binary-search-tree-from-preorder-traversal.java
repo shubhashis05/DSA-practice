edb1709ad58e453b2ebb4d2c6292a4717fa0ced5
Shubhashis Das <139637882+shubhashis05@users.noreply.github.com>
2026-09-09 17:07:01 +0530

    Time: 1 ms (12.30%) | Memory: 43.3 MB (87.77%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -1,53 +1,26 @@
 class Solution {
-    public static TreeNode helper(int[] pre, int[] in,
-                                  int lop, int hip,
-                                  int loi, int hii) {
-
-        if(lop > hip || loi > hii) return null;
-
-        int nVal = pre[lop];
-        int pos = 0;
-
-        for(int i = loi; i <= hii; i++){
-            if(in[i] == nVal){
-                pos = i;
-                break;
-            }
-        }
-
-        // Number of nodes in left subtree
-        int leftSize = pos - loi;
-
-        TreeNode node = new TreeNode(nVal);
-
-        // Left subtree
-        node.left = helper(pre, in,
-                           lop + 1,
-                           lop + leftSize,
-                           loi,
-                           pos - 1);
-
-        // Right subtree
-        node.right = helper(pre, in,
-                            lop + leftSize + 1,
-                            hip,
-                            pos + 1,
-                            hii);
-
-        return node;
+    public int[] array(ArrayList<Integer> list){
+        int[] arr = new int[list.size()];
+        for(int i = 0 ; i < arr.length ; i++)
+            arr[i] = list.get(i);
+        return arr;
     }
-
-    public TreeNode bstFromPreorder(int[] preorder) {
-
-        int n = preorder.length;
-
-        int[] inorder = preorder.clone();
-        Arrays.sort(inorder);
-
-        TreeNode root = helper(preorder, inorder,
-                               0, n - 1,
-                               0, n - 1);
-
+    public TreeNode helper(int[] pre){
+        if(pre.length == 0) return null;
+        int x = pre[0];
+        TreeNode root = new TreeNode(x);
+        ArrayList<Integer> low = new ArrayList<>();
+        ArrayList<Integer> hi = new ArrayList<>();
+        for(int i = 1 ; i < pre.length ; i++){
+            if(pre[i]>x) hi.add(pre[i]);
+            else low.add(pre[i]);
+        }
+        root.left = helper(array(low));
+        root.right = helper(array(hi));
+        return root;
+    }
+    public TreeNode bstFromPreorder(int[] pre) {
+        TreeNode root = helper(pre);
         return root;
     }
 }

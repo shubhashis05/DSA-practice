4c7bca3701696a591d48263e8f3b3d0f9f705398
Shubhashis Das <139637882+shubhashis05@users.noreply.github.com>
2026-09-26 13:46:19 +0530

    Time: 2 ms (72.78%) | Memory: 63.4 MB (7.03%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -3,11 +3,8 @@ class Solution {
         int count = 0 ; 
         int candidate = 0 ;
         for(int i = 0 ; i < arr.length ; i++){
-            if(count == 0){
+            if(count == 0 || candidate == arr[i]){
                 candidate = arr[i];
-                count++;
-            }
-            else if(candidate == arr[i]){
                 count++;
             }
             else{
